c441d64284259c980918115a777efface8f0dc37
Rtificer <acardgame@protonmail.com>
2022-11-27 15:06:35 +0100

    first edit
    based on "tudortodorescu/chess"

---
+++ config/piecesImages.config.js
@@ -0,0 +1,19 @@
+
+export const piecesImages = {
+    'white_pawn': './img/WhitePawn.png',
+    'white_rook': './img/WhiteRook.png',
+    'white_knight': './img/WhiteKnight.png',
+    'white_bishop': './img/WhiteBishop.png',
+    'white_toad': './img/WhiteToad.png',
+    'white_fool': './img/WhiteFool.png',
+    'white_king': './img/WhiteKing.png',
+    'white_queen': './img/WhiteQueen.png',
+    'black_pawn': './img/BlackPawn.png',
+    'black_rook': './img/BlackRook.png',
+    'black_knight': './img/BlackKnight.png',
+    'black_bishop': './img/BlackBishop.png',
+    'black_toad': './img/BlackToad.png',
+    'black_fool': './img/BlackFool.png',
+    'black_king': './img/BlackKing.png',
+    'black_queen': './img/BlackQueen.png',
+}
